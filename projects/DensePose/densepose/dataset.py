b660cf5e622cb7b764e9652a823815cb91949a45
conansherry <conansherry@vip.qq.com>
2019-11-05 17:52:27 +0800

    redo other change

--- projects/DensePose/densepose/dataset.py
+++ projects/DensePose/densepose/dataset.py
@@ -8,7 +8,7 @@ from detectron2.data.datasets import load_coco_json
 def get_densepose_metadata():
     meta = {
         "thing_classes": ["person"],
-        "densepose_transform_src": "datasets/coco/uv/UV_symmetry_transforms.mat",
+        "densepose_transform_src": "detectron2://densepose/UV_symmetry_transforms.mat",
         "densepose_smpl_subdiv": "detectron2://densepose/SMPL_subdiv.mat",
         "densepose_smpl_subdiv_transform": "detectron2://densepose/SMPL_SUBDIV_TRANSFORM.mat",
     }
@@ -16,15 +16,15 @@ def get_densepose_metadata():
 
 
 SPLITS = {
-    "densepose_coco_2014_train": ("coco/train2014", "coco/annotations/densepose_coco_2014_train.json"),
-    "densepose_coco_2014_minival": ("coco/val2014", "coco/annotations/densepose_coco_2014_minival.json"),
+    "densepose_coco_2014_train": ("coco/train2014", "coco/annotations/densepose_train2014.json"),
+    "densepose_coco_2014_minival": ("coco/val2014", "coco/annotations/densepose_minival2014.json"),
     "densepose_coco_2014_minival_100": (
         "coco/val2014",
-        "coco/annotations/densepose_coco_2014_minival.json",
+        "coco/annotations/densepose_minival2014_100.json",
     ),
     "densepose_coco_2014_valminusminival": (
         "coco/val2014",
-        "coco/annotations/densepose_coco_2014_valminusminival.json",
+        "coco/annotations/densepose_valminusminival2014.json",
     ),
 }
 
